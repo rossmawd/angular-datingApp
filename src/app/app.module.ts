ec96f293d7096272b0312bc18741072e26a0853b
Ross Mawdsley <rossmawd@gmail.com>
2020-05-03 18:04:37 +0100

    making call to the API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
@@ -10,7 +12,8 @@ import { ValueComponent } from './value/value.component';
       ValueComponent
    ],
    imports: [
-      BrowserModule
+      BrowserModule,
+      HttpClientModule
    ],
    providers: [],
    bootstrap: [
