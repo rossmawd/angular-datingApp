cb67a4710c08006bbb62a520cbc9091ea0dd1410
Ross Mawdsley <rossmawd@gmail.com>
2020-06-05 11:10:24 +0100

    displaying values from database

--- src/app/value/value.component.ts
+++ src/app/value/value.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Subscription } from 'rxjs';
 
+
 @Component({
   selector: 'app-value',
   templateUrl: './value.component.html',
@@ -16,15 +17,16 @@ export class ValueComponent implements OnInit {
     this.getValues();
   }
 
-  getValues(){
-    this.http.get('http://localhost:5000/api/values').subscribe(resp => {
-      this.values = resp;
-    }, error => {
-      console.log(error);
-    }, () => {
-      console.log('The subscription is complete');
-    }
-    );
+  getValues() {
+    this.http.get('http://localhost:5000/api/values')
+      .subscribe(resp => {
+        this.values = resp;
+      }, error => {
+        console.log(error);
+      }, () => {
+        console.log('The subscription is complete');
+      }
+      );
   }
 
 }
